93d4b43d3a367c51141a2201d100047188e56cc9
Gerald <stonnersdu39@gmail.com>
2020-09-30 10:54:34 +0200

    exo 1 presque fini

---
+++ td2js/JavaS.js
@@ -0,0 +1,57 @@
+function titre(){
+let h2 = document.createElement("h2");
+h2.innerHTML="Article 0 - Doubler <br>";
+document.body.insertBefore(h2,
+document.body.firstChild);
+}
+titre();
+function paragraphe(){
+    let p = document.createElement("p");
+    p.innerHTML="Il est interdit de doubler sous peine de discalifiquation";
+    document.body.insertBefore(p,
+        document.body.children[1]);
+}
+paragraphe();
+
+function majuscule(){
+let tab= document.getElementsByTagName("h2");
+for (let i=0;i<tab.length;i++){
+    tab[i].style.textTransform="uppercase";
+}
+}
+function changeNumArt(){
+
+    let b;
+    let tab=document.getElementsByTagName("h2");
+    for (i=0;i<tab.length;i++){
+
+        let res="";
+        b=tab[i].innerHTML.split(' ');
+        b[1]=parseInt(b[1])+1;
+        for(y=0;y<b.length;y++){
+            res=res+  " "+ b[y];
+        }
+        let li1 = document.createElement("h2");
+        li1.innerHTML = res;
+        document.body.replaceChild(li1,tab[i]);
+    }
+}
+changeNumArt();
+majuscule();
+
+function colorationFond(){
+    let tab= document.getElementsByTagName("h2");
+    let tab2= document.getElementsByTagName("p");
+
+    for (let i=0;i<tab.length;i++){
+        if (i%2===0){
+            tab[i].style.backgroundColor = "#0000ff";
+            tab[i].nextElementSibling.style.backgroundColor = "#ff0000";
+            //tab2[i].style.backgroundColor = "#00ff00";
+
+        }
+    }
+
+
+}
+colorationFond();
